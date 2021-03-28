1a080a53a52a3c13cfb9a33f4835fdf60113f799
Jesse Wenger <wengerj@gmail.com>
2021-03-28 00:22:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,6 +12,6 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << row
+      column_names << row["name"]
   end
 end
