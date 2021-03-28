4ad3293e4c3e6bbefadac938cd5a1b77b7acb410
Jesse Wenger <wengerj@gmail.com>
2021-03-28 00:54:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -45,8 +45,8 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = "SELECT * FROM "
   end
 end
 
