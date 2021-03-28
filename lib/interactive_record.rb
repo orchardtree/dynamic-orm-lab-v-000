cd794e990cd41ba907d046edbe1689a98b4033c6
Jesse Wenger <wengerj@gmail.com>
2021-03-28 00:41:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,7 +34,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "'#{send.col_name}'" unless col_name.nil
+      values << "'#{send(col_name)}'" unless col_name.nil
   end
 end
 
