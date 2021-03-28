9f592c48f7800800f6f028b230da9a61e384222e
Jesse Wenger <wengerj@gmail.com>
2021-03-28 01:03:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,8 +52,8 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    DB[:conn].execute(sql, attribute[value])
+    #sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    #DB[:conn].execute(sql, attribute[value])
     binding.pry
   end
 end
