7e150141d19f611b26976b1227d734e74311e6ac
Jesse Wenger <wengerj@gmail.com>
2021-03-28 00:14:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
   
+  def self.column_names
+    DB[:conn].results_as_hash = true
+  end
 end
